221424d77d54ac564607c3ff6c3eb9276b658cdb
Teenuashwanth SK <139578861+Teenuashwanth@users.noreply.github.com>
2023-12-06 17:56:44 +0530

    Add files via upload

---
+++ CreateObject.java
@@ -0,0 +1,14 @@
+public class CreateObject
+{    
+void show()    
+{    
+System.out.println("Welcome to javaTpoint");    
+}    
+public static void main(String[] args)   
+{    
+
+CreateObjectExample1 obj = new CreateObjectExample1();   
+  
+obj.show();    
+}    
+}   
